44c207485c738d19840c3ce5c08fd977e2aad10b
zirius666 <zion.system@hotmail.com>
2022-12-27 14:47:05 +0000

    Deploying to main from @ zirius666/testghpages@fcb83ddce9b30c9d64ce415829626bf961db1b9f 🚀

--- libs/shared-components/src/lib/shared-components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TestComponentComponent } from './test-component/test-component.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [TestComponentComponent],
-})
-export class SharedComponentsModule {}
