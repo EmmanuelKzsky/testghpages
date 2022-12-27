decf1a4fef3ce34a0ca4401cb717796d11fac5b6
Emmanuel Castro <emmanuel.castro.pantoja@gmail.com>
2022-12-27 09:02:08 -0600

    Added changes for gh actions

---
+++ libs/shared-components/src/lib/shared-components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestComponentComponent } from './test-component/test-component.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TestComponentComponent],
+})
+export class SharedComponentsModule {}
